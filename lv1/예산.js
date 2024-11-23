f6031b7fcb3e44ed85b4b200cc7cf748c082edb4
uzyhu <dnwlgn0807@naver.com>
2024-11-23 18:05:11 +0900

    add : new (reduce, sort)

---
+++ lv1/예산.js
@@ -0,0 +1,22 @@
+function solution(d, budget) {
+    var answer = 0;
+    let cost = 0;
+    for(let i of d.sort((a,b) => a-b)) {
+        if(cost+i > budget) {
+            break;
+        }
+        cost += i;
+        answer += 1;
+    }
+    return answer;
+}
+
+// function solution(d, budget) {
+//     return d.sort((a, b) => a - b).reduce((count, price) => {
+//         return count + ((budget -= price) >= 0);
+//     }, 0);
+// }
+
+
+// https://hianna.tistory.com/409 ==> sort()
+// https://miiingo.tistory.com/365 ==> reduce()
