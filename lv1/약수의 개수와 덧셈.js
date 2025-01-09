af897f3ddc5c7f0e8affb07ec580ef1d6df94e5c
uzyhu <dnwlgn0807@naver.com>
2025-01-09 22:02:44 +0900

    fix : 수정

--- lv1/약수의 개수와 덧셈.js
+++ lv1/약수의 개수와 덧셈.js
@@ -29,5 +29,19 @@ function solution(left, right) {
   return answer;
 }
 
+//다른 사람의 풀이2 ...
 
-//다른 사람의 풀이2 ... 
+function solution(left, right) {
+  let answer = 0;
+
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) count++;
+    }
+    if (count % 2) answer -= i;
+    else answer += i;
+  }
+
+  return answer;
+}
