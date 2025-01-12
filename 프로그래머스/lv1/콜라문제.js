321b249bd9339cecf1e0ed10a0666568a4de9bc7
uzyhu <dnwlgn0807@naver.com>
2025-01-12 23:04:30 +0900

    add : 1/12

---
+++ 프로그래머스/lv1/콜라문제.js
@@ -0,0 +1,16 @@
+function solution(a, b, n) {
+    var answer = 0;
+    while (n>=a) {
+        if(n%a!=0) {
+            answer += b*(Math.floor(n/a));
+            n = n%a + b*(Math.floor(n/a));
+        }
+        else {
+            answer += b*(n/a);
+            n = b*(n/a);
+        }
+    }
+    return answer;
+}
+
+// floor, ceil, round 
