6eaa0a69e60cf4ef2e618d2caef3b34708493068
uzyhu <dnwlgn0807@naver.com>
2025-01-16 18:54:24 +0900

    add : 25/1/16 (2016년)

---
+++ 프로그래머스/lv1/2016년.js
@@ -0,0 +1,24 @@
+function solution(a, b) {
+    let arr = ['THU','FRI','SAT','SUN','MON','TUE','WED'];
+    let total = b;
+    for(let i=1; i<a; i++) {
+        if(i==8) {
+            total+=31;
+        }
+        else if(i==9) {
+            total+=30;
+        }
+        else if(i==2) {
+            total+=29;
+        }
+        else if(i%2==0) {
+            total+=30;
+        }
+        else {
+            total+=31;
+        }
+    }
+    return arr[total%7];
+}
+
+//Date 객체 사용하면 쉬움
