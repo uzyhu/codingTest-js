1612983369a4c5a518cc5d20d918f5b67cca2f44
uzyhu <dnwlgn0807@naver.com>
2025-01-23 01:02:18 +0900

    add : 1/23 (소수찾기, 성공했음!)

--- 프로그래머스/lv1/소수찾기.js
+++ 프로그래머스/lv1/소수찾기.js
@@ -14,3 +14,40 @@ function solution(n) {
 }
 
 //효율성 테스트 실패
+//아래는 최종 성공 코드
+
+function solution(n) {
+    var answer = 1; // 2 미리 포함
+    for(let i=3; i<=n; i++) {
+        let num = true;
+        if(i%2==0) continue;
+        for(let j=3; j<=Math.sqrt(i); j++) {
+            if(j%2==0) continue;
+            if(i%j == 0) {
+                num = false;
+                break;
+            }
+        }
+        answer += num == true ? 1 : 0;
+    }
+    return answer;
+}
+
+//다른사람의 풀이 ,,, 무슨무슨 체 적용.,,,,,,,,, 이해해야함
+
+function solution(n) {
+    const s = new Set();
+    for(let i=1; i<=n; i+=2){
+        s.add(i);
+    }
+    s.delete(1);
+    s.add(2);
+    for(let j=3; j<Math.sqrt(n); j++){
+        if(s.has(j)){
+             for(let k=j*2; k<=n; k+=j){    
+                s.delete(k);
+             }
+        }
+    }
+    return s.size;
+}
