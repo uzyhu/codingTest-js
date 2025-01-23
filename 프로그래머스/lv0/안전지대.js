82ac9284b268915d70b8c0815a8ed604d79379f5
uzyhu <dnwlgn0807@naver.com>
2025-01-23 17:20:52 +0900

    add : 1/23 (안전지대 , 다른사람의 풀이.. 옵셔널체이닝)

--- 프로그래머스/lv0/안전지대.js
+++ 프로그래머스/lv0/안전지대.js
@@ -71,3 +71,46 @@ function solution(board) {
     board.forEach((a) => a.forEach((b) => answer += b == 0 ? 1 : 0));
     return answer;
 }
+
+//다른 사람의 풀이1
+
+function solution(b) {
+    const directions = [[0,0],[0,1],[0,-1],[1,1],[1,0],[1,-1],[-1,-1],[-1,0],[-1,1]]
+    let bombSet = new Set();
+
+    for(let i = 0; i < b.length; i++) {
+        for(let j = 0; j < b[i].length; j++) {
+            if(b[i][j] == 1) {
+                directions.forEach(el => {
+                    let [nextX, nextY] = el;
+                    [nextX, nextY] = [i+nextX, j+nextY];
+                    if(nextX >= 0 && nextX < b.length && nextY >= 0 && nextY < b[i].length) {
+                        bombSet.add(nextX+' '+nextY);
+                    }
+                })
+            }
+        }
+    }
+    return b.length * b[0].length - bombSet.size;
+}
+
+//다른 사람의 풀이2
+function solution(board) {
+
+    for (let y = 0; y < board.length; y++)
+        for (let x = 0; x < board[0].length; x++) {
+
+            if (board[y][x] == 1) {
+                for (let i = -1; i <= 1; i++) {
+                    for (let j = -1; j <= 1; j++) {
+                        if (board[y + i]?.[x + j] === 0) 
+                            board[y + i][x + j] = 2
+                    }
+                }
+            }
+
+        }
+
+    return [...board.join()].filter(v => v == '0').length;
+
+}
